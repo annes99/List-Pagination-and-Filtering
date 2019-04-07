4d4dacac27bcccf189c9f9b9df2ff2d19faa91dc
Annes S <47981031+annes99@users.noreply.github.com>
2019-04-07 22:03:27 +0700

    add showPage & appendPageLinks func

--- js/script.js
+++ js/script.js
@@ -2,49 +2,72 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
+
+const students = document.getElementsByClassName('student-item');
+const itemsToShow = 10;
+
+// func to calculate correct number of page links (max 10 per page)
+const pageNumbers = number => Math.ceil(number / itemsToShow);
+
+// func to display only 10 items per page
+const showPage = (list, page) => {
+  const startIndex = (page * itemsToShow) - itemsToShow;
+  const endIndex = page * itemsToShow;  
+
+  for (let i = 0; i < list.length; i++) {  
+    if ([i] >= startIndex && [i] < endIndex) {
+      list[i].style.display = 'block';
+    } else {
+      list[i].style.display = 'none';
+    }
+  }
+};
+
+const appendPageLinks = list => {
+
+   // create <div> container with nested <ul> tags
+   const div = document.createElement('div');
+   div.setAttribute('class', 'pagination');
+   div.setAttribute('id', 'pageNumbers');
+   const mainDiv = document.querySelector('.page');
+   mainDiv.appendChild(div);
+   const ul = document.createElement('ul');
+   div.appendChild(ul);
    
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
+ 
+   // for every page add one <li> with nested <a> tag to <ul>
+   for(let i = 1; i <= pageNumbers(list.length); i++){
+     const li = document.createElement('li');
+     ul.appendChild(li); 
+     const a = document.createElement('a');
+     li.appendChild(a);
+     // adding number and href to <a> tag
+     a.setAttribute('href', '#');
+     a.textContent = [i];
+     // make first a tag  active
+     const aTag = document.querySelector('a');
+     aTag.classList.add('active');
+   }
+   const a = document.getElementsByTagName('a');
+   // looping over <a> tag first all active tags are removed
+   // then active tag is added to link that was just clicked
+   for(let i = 0; i < a.length; i++) {
+     
+     a[i].addEventListener('click', (event) =>{
+ 
+       for(let i = 0; i < a.length; i++) {
+         a[i].classList.remove('active');
+       } 
+ 
+       let pageNum = a[i].textContent;
+       event.target = a[i].classList.add('active');
+       showPage(list, pageNum);
+     });
+   }
    
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
+ };
 
-
-
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
-
-
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+ // calling showPage & appendPageLinks so when page loads first time
+// it shows info of max 10 students and page links to others if there's more
+showPage(students, 1);
+appendPageLinks(students);
